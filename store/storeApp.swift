843ce816193871d7d1f07f2e307f325793ca40c5
kehinde <bankolek1@gmail.com>
2024-11-02 16:06:03 +0100

    Initial Commit

---
+++ store/storeApp.swift
@@ -0,0 +1,17 @@
+//
+//  storeApp.swift
+//  store
+//
+//  Created by Kehinde Bankole on 02/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct storeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
